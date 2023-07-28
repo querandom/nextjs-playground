90b8f8347e8fb8012d9b23cadd07a36bad35f1f7
Pablo Torres <pablotorrescba@gmail.com>
2023-07-28 14:11:45 -0300

    routing features

---
+++ src/app/not-found.tsx
@@ -0,0 +1,9 @@
+const PageNotFound = () => {
+  return (
+    <div>
+      <h1>Custom 'Page not found' Page</h1>
+    </div>
+  )
+}
+
+export default PageNotFound
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,14 @@
+import Link from 'next/link'
+
 export default function Home() {
   return (
     <div>
       <h1>This is the Home Page</h1>
+      <ul>
+        <li>
+          <Link href="/about">About</Link>
+        </li>
+      </ul>
     </div>
   )
 }
